f2d9020bcb66495dcbacffc7a661782e0ea1079c
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-04-08 15:42:05 +0900

    refactor: 인기 영화 슬라이더 리팩토링

---
+++ src/constants/responsive.js
@@ -0,0 +1,14 @@
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 480 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 480, min: 0 },
+    items: 1,
+  },
+};
--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -1,29 +1,10 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
 import Spinner from "react-bootstrap/Spinner";
-import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import MovieCard from "../MovieCard/MovieCard";
-import "./PopularMovieSlide.style.css";
-
-const responsive = {
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 480 },
-    items: 2,
-    slidesToSlide: 1,
-  },
-  mobile: {
-    breakpoint: { max: 480, min: 0 },
-    items: 1,
-    slidesToSlide: 1,
-  },
-};
+import MovieSlider from "../../../../common/MovieSlider/MovieSlider";
+import { responsive } from "../../../../constants/responsive";
+import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovies";
 
 const PopularMovieSlide = () => {
   const { data, isLoading, isError, error } = usePopularMoviesQuery();
@@ -36,31 +17,11 @@ const PopularMovieSlide = () => {
   }
   return (
     <div>
-      <h3>Top Popular Movies</h3>
-      <Carousel
-        infinite={true}
-        centerMode={true}
-        itemClass="movie-slider p-1"
-        containerClass="carousel-container"
+      <MovieSlider
+        title="Top Popular Movies"
+        movies={data.results}
         responsive={responsive}
-        // swipeable={false}
-        // draggable={false}
-        // showDots={true}
-        // ssr={true} // means to render carousel on server-side.
-        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-        // autoPlaySpeed={1000}
-        // keyBoardControl={true}
-        // customTransition="all .5"
-        // transitionDuration={500}
-        removeArrowOnDeviceType={["mobile"]}
-        // deviceType={this.props.deviceType}
-        // dotListClass="custom-dot-list-style"
-      >
-        {data.results.map((movie, index) => (
-          <MovieCard movie={movie} key={index} />
-        ))}
-      </Carousel>
-      ;
+      />
     </div>
   );
 };
