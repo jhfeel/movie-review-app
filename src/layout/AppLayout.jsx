7367440c1a6dad2b358efa4cd5cac83e32baa246
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-04-10 09:39:57 +0900

    feat: 영화 검색 기능 구현

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Outlet } from "react-router-dom";
-import "./AppLayout.style.css";
+import { Outlet, useNavigate } from "react-router-dom";
 import netflixLogo from "./../assets/netflix-Logo.svg";
+import "./AppLayout.style.css";
 
 const AppLayout = () => {
+  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate();
+
+  const searchByKeyword = (e) => {
+    e.preventDefault();
+    navigate(`movies?q=${keyword}`);
+    setKeyword("");
+  };
+
   return (
     <div>
       <Navbar
@@ -34,12 +43,14 @@ const AppLayout = () => {
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
-            <Form className="d-flex">
+            <Form className="d-flex" onSubmit={searchByKeyword}>
               <Form.Control
                 type="search"
                 placeholder="Search"
                 className="me-2"
                 aria-label="Search"
+                value={keyword}
+                onChange={(e) => setKeyword(e.target.value)}
               />
               <Button variant="outline-danger">Search</Button>
             </Form>
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
+import { useSearchParams } from "react-router-dom";
+import { Container, Row, Col, Spinner, Alert } from "react-bootstrap";
+import MovieCard from "../../common/MovieCard/MovieCard";
 
 const MoviePage = () => {
-  return <div>movie</div>;
+  const [query, setQuery] = useSearchParams();
+  const keyword = query.get("q");
+
+  const { data, isLoading, isError, error } = useSearchMovieQuery({ keyword });
+  console.log("dad", data);
+
+  if (isLoading) {
+    return <Spinner animation="border" variant="danger" role="status" />;
+  }
+  if (isError) {
+    return <Alert variant="danger">{error.message}</Alert>;
+  }
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col lg={12} xs={12}>
+            <Row>
+              {data?.map((movie, index) => (
+                <Col key={index} lg={4} md={6} xs={6}>
+                  <MovieCard movie={movie} />
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default MoviePage;
